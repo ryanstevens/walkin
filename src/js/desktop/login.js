adca066975e7485e8254c952a89ba74cc7576fc4
Ryan Stevens <stevens.ryan@gmail.com>
2014-05-17 15:34:46 -0700

    user stuff

--- src/js/desktop/login.js
+++ src/js/desktop/login.js
@@ -1,24 +1,28 @@
 $(function (argument) {
 
-function tryLogin (response) {
-  if (response && response.status == 'connected') {
-    FB.api('me', function (user) {
-      console.log('Got /me data:', user);
-    });
-  } else {
-    FB.login(tryLogin);
+  function tryLogin (response) {
+    if (response && response.status == 'connected') {
+      FB.api('me', function (user) {
+        user.authData = response.authResponse;
+        console.log('Got /me data:', user);
+      });
+    } else {
+      FB.login(tryLogin, {
+        scope : 'email', 
+        return_scopes: true
+      });
+    }
   }
-}
 
-FB.init({
-  appId: '1504962113057983',
-  status: true,
-  cookie: true,
-  xfbml: false
-});
+  FB.init({
+    appId: '1504962113057983',
+    status: true,
+    cookie: true,
+    xfbml: false
+  });
 
-$('.login').click(function (e) {
-  FB.getLoginStatus(tryLogin);
-});
+  $('.login').click(function (e) {
+    FB.getLoginStatus(tryLogin);
+  });
 
 });
--- test/test.js
+++ test/test.js
@@ -1,5 +1,20 @@
 var test = require('tap').test;
+var user = require('../lib/user');
 
 test('Parse', function(t) {
-  t.end();
+
+  user.createUser({
+    first_name: "Ryan"
+    ,gender: "male"
+    ,id: "10152017615725566"
+    ,last_name: "Stevens"
+    ,link: "https://www.facebook.com/app_scoped_user_id/10152017615725566/"
+    ,locale: "en_US"
+    ,name: "Ryan Stevens"
+    ,timezone: -7
+    ,updated_time: "2014-02-07T18:41:55+0000"
+    , verified: true}, function(err, user) {
+      if (err) return console.error('ERROR', err);
+      console.log(user);
+    });
 });
