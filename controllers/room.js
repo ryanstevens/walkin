35a41e43c8ee1a47f3475484abd77304aa0f7eca
Ryan Stevens <stevens.ryan@gmail.com>
2014-05-18 10:35:34 -0700

    fail gracefully

--- controllers/room.js
+++ controllers/room.js
@@ -12,6 +12,10 @@ module.exports = function (req, res, template, viewData) {
   viewData = viewData || { title: capitalize(route) };
 
   room.getBySlug(req.params.name).done(function(room) {
+    if (!room) {
+      res.redirect(301, '/rooms');
+      return;
+    }
     viewData.room = room;
     res.render(template, viewData);
   });
