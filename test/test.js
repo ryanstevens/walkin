8a90d3de246bc5e0e0e1be19bfbc50921aa18402
Ryan Stevens <stevens.ryan@gmail.com>
2014-05-17 17:28:30 -0700

    user things

--- test/test.js
+++ test/test.js
@@ -6,15 +6,21 @@ test('Parse', function(t) {
   user.createUser({
     first_name: "Ryan"
     ,gender: "male"
-    ,id: "10152017615725566"
+    ,fb_user_id: "10152017615725566"
     ,last_name: "Stevens"
     ,link: "https://www.facebook.com/app_scoped_user_id/10152017615725566/"
     ,locale: "en_US"
     ,name: "Ryan Stevens"
     ,timezone: -7
     ,updated_time: "2014-02-07T18:41:55+0000"
-    , verified: true}, function(err, user) {
-      if (err) return console.error('ERROR', err);
-      console.log(user);
-    });
+    , verified: true}).done(function(userModel) {
+
+      user.makeSession(userModel).done(function(session) {
+
+        user.getUserFromSession(session.id).done(function() {
+          console.log("Session", session.id);
+        });
+      })
+
+    })
 });
